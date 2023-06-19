13e6f1319db58434bc6e37281dcdbdc79ed84288
DanyelC <danyel.clinario@gmail.com>
2023-06-19 00:34:51 -0300

    bug fix, link, music_artist search

--- flask/website/music/auth.py
+++ flask/website/music/auth.py
@@ -2,7 +2,7 @@ import tekore as tk
 
 def authorize():
     """Pega as credenciais e autoriza o acesso. Retorna um objeto que nos permite acessar a API."""
-    CLIENT_ID = "f6ba63651f854785af79bde32ef60c26"
-    CLIENT_SECRET = "dedda806113041afa823350135d90e2d"
+    CLIENT_ID = "ec851854db1c4433a4bb215d215893ed"
+    CLIENT_SECRET = "709825c655ee4c39b0dd31d17a670217"
     app_token = tk.request_client_token(CLIENT_ID, CLIENT_SECRET)
     return tk.Spotify(app_token)
--- flask/website/views.py
+++ flask/website/views.py
@@ -10,38 +10,42 @@ views = Blueprint('views', __name__)
 # página inicial
 @views.route('/', methods=['GET', 'POST'])
 def home():
+    track_id = None
+    sp = authorize()
     if request.method == 'POST':
+
         track_id = request.form.get('track_id')
 
         if 'spotify.com' in track_id:
             track_id = get_id_from_link(track_id) #track_id.split('/')[-1]
+        else:
+            try: 
+                sp.track(track_id)
+            except:
+                try:
+                    track_id = get_id_from_name(track_id)
+                    sp.track(track_id)
+                except:
+                    flash('Invalid track ID. Please try again.', category='error')
+                    return render_template('base.html')
+
 
         variables_to_dist = list(request.form)
         variables_to_dist.remove('track_id')
-
-    sp = authorize()
-    
-    try: 
-        sp.track(track_id)
-    except:
-        try:
-            track_id = get_id_from_name(track_id)
-            sp.track(track_id)
-        except:
-            flash('Invalid track ID. Please try again.', category='error')
-            return render_template('base.html')
     
 
     
-    link = f"https://open.spotify.com/embed/track/{track_id}?utm_source=generator"
+        link = f"https://open.spotify.com/embed/track/{track_id}?utm_source=generator"
 
-    data = pd.read_csv('https://raw.githubusercontent.com/SylvioMello/MusicMentor/main/flask/website/music/spotify_data/data.csv')
-    recommendations = recommend(track_id, data, sp, variables_to_dist = variables_to_dist)['id'].values.tolist()
-    rec_links = []
+        data = pd.read_csv('https://raw.githubusercontent.com/SylvioMello/MusicMentor/main/flask/website/music/spotify_data/data.csv')
+        recommendations = recommend(track_id, data, sp, variables_to_dist = variables_to_dist)['id'].values.tolist()
+        rec_links = []
 
-    for rec_id in recommendations:
-        rec_link = f"https://open.spotify.com/embed/track/{rec_id}?utm_source=generator"
-        rec_links.append(rec_link)
+        for rec_id in recommendations:
+            rec_link = f"https://open.spotify.com/embed/track/{rec_id}?utm_source=generator"
+            rec_links.append(rec_link)
+        
+
+        return render_template('base.html', your_song=link, recommendations_links=rec_links)
     
-
-    return render_template('base.html', your_song=link, recommendations_links=rec_links)
+    return render_template('base.html')
